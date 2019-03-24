0fe16ad14bd2756b36c79d2efd1a25f8ff2d4bb7
Frank <fsr32@cornell.edu>
2019-03-24 02:26:24 -0400

    cleaning up

--- queries.js
+++ queries.js
@@ -1,29 +1,30 @@
 const { Pool, Client } = require('pg');
 var config = require('./db-config.js');
-
 var getUsers;
+
 if (config.production){
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true
   });
+
   client.connect();
   getUsers = (req, response) => {
     let pool = new Pool({
       connectionString: process.env.DATABASE_URL,
       ssl: true
-    })
+    });
     
     pool.connect().then( client => {
       client.query('SELECT * FROM team_members;', (err, res) => {
         if (err) {
           console.log(err);
         }
-        response.status(200).json(res.rows)
+        response.status(200).json(res.rows);
       })
     })
     .catch(e=> {
-      console.log(e)
+      console.log(e);
     })
   };
 
@@ -35,14 +36,12 @@ if (config.production){
     password: config.local_db.password,
     host: config.local_db.host
   }); 
+  
   getUsers = (request, response) => {
     pool.query('SELECT * FROM team_members', (error, results) => {
       if (error) {
-        console.log("ERROR CONNECTING TO CLIENT")
         console.log(error);
       }
-      console.log("ERROR CHANGING RES INTO JSON");
-      console.log(results);
       response.status(200).json(results.rows)
     })
   };
--- server.js
+++ server.js
@@ -1,43 +1,18 @@
 const express = require('express');
 const path = require('path');
 const db = require('./queries')
-
 const app = express();
-
-// This is just for Monday, its just to display
-// our names and netIds...I could easily but this on 
-// the front end but now you guys can see how data 
-// from the backend is transferred to front end
-const team = {
-  'Frank Rodriguez': 'Fsr32',
-  'Aditya Jha': 'Aj377',
-  'Jacob Mathai': 'Jm2463',
-  'Orvill De La Torre':'Od52',
-  'Tharun Sankur':'Tps87'
-}
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Endpoint definitions
-app.get('/api/passwords', (req, res) => {
-
-  // Return them as json
-  res.json(team);
-
-  console.log(`Sent team names`);
-});
-
-
+/* Endpoints */
 app.get('/api/users', db.getUsers)
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-
 console.log(`Server listening on ${port}`);
