046aceb76f12936fe244c0f4245daef320bb7085
Frank <fsr32@cornell.edu>
2019-03-23 23:33:41 -0400

    Adding config example

---
+++ db-config.js
@@ -0,0 +1,12 @@
+  
+var config = {
+    db:{
+      host:"localhost",
+      database:"api",
+      user:"me",
+      password:"password",
+      port: "5432"
+    },
+  }
+  
+module.exports = config;
