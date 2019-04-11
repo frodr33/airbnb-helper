efc85df9b548d633748ca793c95a11a3a053ec02
Frank <fsr32@cornell.edu>
2019-04-10 21:31:41 -0400

    example of how to call python script from javascript

---
+++ machine-learning/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import sys
+
+arg1 = int(sys.argv[1])
+arg2 = int(sys.argv[2])
+arg3 = int(sys.argv[3])
+x = [arg1,arg2,arg3]
+
+print(x)
+sys.stdout.flush()
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const db = require('./queries')
 const app = express();
 const http = require("http");
+const { spawn } = require('child_process');
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -11,9 +12,24 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // Commenting out the database stuff for now
 // app.get('/api/users', db.getUsers)
 
+app.get("/api", (req, res) => {
+  res.json("HELLO WORLD");
+})
+
+app.get("/api/pyTest", (req, res) => {
+  const pyProgram = spawn('Python', ['./machine-learning/test.py', 1, 2, 3])
+
+  pyProgram.stdout.on('data', (chunk) => {
+    var textChunk = JSON.parse(chunk.toString('utf8'));
+    res.json(textChunk);
+    // res.end();
+  })
+})
+
 app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
