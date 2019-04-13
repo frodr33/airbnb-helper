4b20ecb077e2b6c870edff1e377411c364965458
Tharun Sankar <tharun.sankar@gmail.com>
2019-04-13 12:27:12 -0400

    keywords fixes

--- machine-learning/keywords.py
+++ machine-learning/keywords.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import string
 import os.path
 import pickle
+import numpy as np
 
 class TFIDF(object):
     # listings_file is path to SUMMARY listings.csv
@@ -34,8 +35,8 @@ class TFIDF(object):
                 for w in tokens:
                     dfs[w] += 1
 
-        n_docs = len(self.review_data['comments'])
-        with open('idfs.pickle', 'w') as f:
+        n_docs = len(self.review_df['comments'])
+        with open('idfs.pickle', 'wb') as f:
             pickle.dump({w: np.log(n_docs / df) for w, df in dfs.items()}, f)
 
     def get_keywords(self, listing_id):
@@ -45,7 +46,7 @@ class TFIDF(object):
         tfs = defaultdict(int)
         for review in listings['comments']:
             if type(review) == str:
-                review_no_punc = review.translate(None, string.punctuation)
+                review_no_punc = review.translate(str.maketrans(dict.fromkeys(string.punctuation)))
                 tokens = nltk.word_tokenize(review_no_punc)
                 tokens = [w for w in tokens if w not in self.stopwords]
                 for w in tokens:
@@ -56,18 +57,18 @@ class TFIDF(object):
         # don't give host name as keyword
         l_df = self.listing_df
         host_name = str(l_df.loc[l_df['id'] == listing_id]['host_name'])
-        tf_idfs = list(filter(lambda (w, _): host_name not in w, tf_idfs))
+        tf_idfs = list(filter(lambda x: host_name not in x[0], tf_idfs))
 
-        tf_idfs = sorted(tf_idfs, key=lambda x: x[1], reverse=True)[:5]
+        tf_idfs = sorted(tf_idfs, key=lambda x: x[1], reverse=True)[:20]
 
-        return [x[1] for x in tf_idfs]
+        return [x[0] for x in tf_idfs]
 
     def gen_keyword_dict(self):
         id_keywords = defaultdict(list)
         for listing_id in self.listing_df['id']:
             id_keywords[listing_id] += self.get_keywords(listing_id)
 
-        with open('id_keywords.pickle', 'w') as f:
+        with open('id_keywords.pickle', 'wb') as f:
             pickle.dump(id_keywords, f)
 
         return id_keywords
@@ -90,6 +91,6 @@ def rank_listings(listings, query):
 
 
 if __name__ == '__main__':
-    K = TFIDF('~/CS4300/reviews.csv')
+    K = TFIDF('~/CS4300/reviews.csv', '~/CS4300/listings.csv')
     print(K.get_keywords(2595))
     K.gen_keyword_dict()
