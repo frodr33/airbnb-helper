d396249d6da09efa500c70e753a1b66e36d127b8
Aditya Jha <42524187+adifun@users.noreply.github.com>
2019-04-22 15:40:59 -0400

    Query Yelp with a search term, latitude, longitude
    returns a ranked list. Uses the word_sentiment pickle for a dictionary of words with positive and negative association. 
    Example output: 
    [(2.6666666666666665, 'Westfield Station Cafe', 'https://s3-media3.fl.yelpcdn.com/bphoto/aB6niM5CwimfnyVoU5VHEQ/o.jpg', 'Breakfast & Brunch', (40.648647, -74.34652)), 
    (2.222222222222222, 'Cross Bronx Pizzeria', 'https://s3-media4.fl.yelpcdn.com/bphoto/D7O4pQop6PjcRQpZY6_SvQ/o.jpg', 'Pizza', (40.830034, 73.850772)), 
    (1.888888888888889, 'Bendix Diner', 'https://s3-media2.fl.yelpcdn.com/bphoto/MBPxFLTya9bSAaUS4QaOow/o.jpg', 'Diners', (40.864891, 74.064074))]

---
+++ machine-learning/query_yelp.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+"""
+This program demonstrates the capability of the Yelp Fusion API
+by using the Search API to query for businesses by a search term and location,
+and the Business API to query additional information about the top result
+from the search query.
+
+Please refer to http://www.yelp.com/developers/v3/documentation for the API
+documentation.
+
+This program requires the Python requests library, which you can install via:
+`pip install -r requirements.txt`.
+
+"""
+from __future__ import print_function
+
+import argparse
+import json
+import requests
+import sys
+import urllib
+from urllib.error import HTTPError
+from urllib.parse import quote
+from urllib.parse import urlencode
+from decimal import Decimal
+import pickle 
+
+#Basic stuff to query the API 
+API_KEY = 'rKs47AtMFAKFD1bHjlNSfEdONcInTie98nkMuR-_x0wKDoX19IX70LBmNy98UIK2mBdBdiQisnoLrukq4bzGvsacGdbGPfwq9h6t0rulHOlM2w8XLd8MgB4MRr68XHYx'
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+
+#Dictionary to classify sentiment
+word_sentiment_dictionary = pickle.load(open("word_sentiment.pickle", "rb" ))
+
+
+# Search default because the Free API Limit is 5000 returned queries
+SEARCH_LIMIT = 5
+
+
+
+def main():
+    try:
+        #Sample Latitude and Longitude Data
+        term = 'dinner' 
+        lat = Decimal(37.782907)
+        longi  = Decimal(-122.418898)
+        #Radius Value
+
+        #Sample query
+        list_of_ranked_results = query_api_lat_Long(term, lat, longi)
+
+    except HTTPError as error:
+        sys.exit(
+            'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
+                error.code,
+                error.url,
+                error.read(),
+            )
+        )
+
+def request(host, path, api_key, url_params=None):
+    """Given your API_KEY, send a GET request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+
+    Returns:
+        dict: The JSON response from the request.
+
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    #print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+def search_with_lat_long(api_key, term, latitude, longitude, radius):
+    """Query the Search API by a search term and location.
+
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'latitude': latitude,
+        'longitude': longitude,
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+
+#Reviews
+def get_reviews_for_business(api_key, business_id, current_rating):
+    reviews_path = BUSINESS_PATH+business_id+'/reviews'
+    average_rating = 0.0
+    overall_sentiment = 0.0
+    request_val = request(API_HOST, reviews_path, api_key)
+    reviews = request_val['reviews']
+    #print (reviews)
+    for value in reviews:
+        #print ('This is the value '+ str(value))
+        positive_word_count = 0.0
+        negative_word_count = 0.0
+        average_rating += value['rating']
+        text_review = value['text']
+        split_of_words = text_review.split()
+        for word in text_review.split():
+            if word in word_sentiment_dictionary:
+                if (word_sentiment_dictionary[word] == 1):
+                    positive_word_count +=1
+                else:
+                    negative_word_count +=1
+        difference_in_sentiment = positive_word_count-negative_word_count
+        total_value = difference_in_sentiment/(len(split_of_words))
+        overall_sentiment += total_value
+
+    average_sentiment = overall_sentiment/3.0
+    aggregate_rating = average_rating/3.0
+    total_rating = create_business_value(current_rating, aggregate_rating, average_sentiment)
+    return total_rating
+
+
+def create_business_value(overall_star_ranking, average_ranking_review, review_sentiment):
+    sentiment_points = 0
+    if (review_sentiment > 0.25):
+        sentiment_points = 1
+    elif (review_sentiment >0):
+        sentiment_points = 0.5
+    elif (review_sentiment <0):
+        sentiment_points =  -0.5
+    elif  (review_sentiment < -0.25):
+        sentiment_points = -1
+
+    total_ranking = overall_star_ranking+(0.5*average_ranking_review)+sentiment_points
+    average_ranking = total_ranking/3.0
+    return average_ranking
+
+   # print (request_val)
+
+def get_business(api_key, business_id):
+    """Query the Business API by a business ID.
+
+    Args:
+        business_id (str): The ID of the business to query.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, api_key) 
+
+def query_api_lat_Long(term, latitude, longitude, radius= 40000):
+    """Queries the API by the input values from the user.
+
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    response = search_with_lat_long(API_KEY, term, Decimal(latitude), Decimal(longitude), radius)
+    #print (response)
+    list_of_results = []
+
+    businesses = response.get('businesses')
+    if not businesses:
+        #print(u'No businesses for {0} in {1} found.'.format(term, location))
+        #print ('no businesses found')
+        return 
+    for value in businesses:
+        business_id = value['id']
+        response = get_business(API_KEY, business_id)
+        #print (response)
+        total_ranking = get_reviews_for_business(API_KEY, business_id, response['rating'])
+        lat_long_tuple = (response['coordinates']['latitude'], response['coordinates']['longitude'])
+        tuple_of_data = (total_ranking, response['name'], response['image_url'], response['categories'][0]['title'], lat_long_tuple)
+        list_of_results.append(tuple_of_data)
+
+
+    list_of_results.sort()
+    list_of_results.reverse()
+
+    return list_of_results
+
+
+
+if __name__ == '__main__':
+    main()
